da22b1733d0fdf4ba8c1299c3133d76673b7c5d1
Mahado <mabdirahman242424@gmail.com>
2025-07-25 10:31:42 -0700

    Added a friend ckass and UI to add Friend

--- BirthdayApp/ContentView.swift
+++ BirthdayApp/ContentView.swift
@@ -8,17 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var friends: [Friend] = [
+        Friend (name: "Poorna", birthday: .now),
+        Friend (name: "Sundus", birthday: .now)
+    ]
+    @State private var newName = ""
+    @State private var newBirthday = Date.now
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List(friends, id: \.name) { friend in
+                HStack {
+                    Text(friend.name)
+                    Spacer()
+                    Text(friend.birthday, format: .dateTime.month(.wide).day().year())
+                }
+                
+            }
+            .navigationTitle("Birthdays")
+            .safeAreaInset(edge: .bottom) {
+                VStack (alignment: .center, spacing: 20){
+                    Text("New Birthday")
+                        .font(.headline)
+                    DatePicker(selection: $newBirthday, in: Date.distantPast...Date.now, dislayedComponents: .date) {
+                        TextField("Name", text: $newName)
+                            .textFieldStyle(.roundedBorder)
+                    }
+                    Button("Save") {
+                        let newFriend = Friend(name: newName, birthday: newBirthday)
+                        friends.append(newFriend)
+                        newName = ""
+                        newBirthday = .now
+                    }
+                    .bold()
+                }
+            }
         }
-        .padding()
     }
 }
-
-#Preview {
-    ContentView()
-}
+        #Preview {
+            ContentView()
+        }
